099845caa09910677108b3412b80e1e6b6caca0a
Shashank Singh <ssingh0271@gmail.com>
2022-11-11 21:46:18 +0530

    patch request updated

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,28 +18,40 @@ const getTask = async (req , res)=>{
     console.log(req.params.id) ;
     try{
         const task = await Task.find({_id : req.params.id}) ;
+        if(!task){
+            return res.status(404).json({msg : `no task with id : ${req.params.id}`});
+        }
         res.json(task).status(200) ;
     }
     catch(error){
-        res.json({message : error}).status(500) ;
+        res.json({message : error}).status(404) ;
     }
 };
 const deleteTask = async(req , res)=>{
     try{
-        await Task.deleteOne({name : req.params.id}) ;
-        res.status(204).json() ;
+        const {id : taskID} = req.params ;
+        const task = await Task.findOneAndDelete({_id : taskID}) ;
+        if(!task){
+            return res.status(404).json({message : `no task with id : ${taskID}`}) ;
+        }
+        console.log(task);
+        res.json({task}).status(204) ;
     }
     catch(error){
-        res.json({message : error}).status(500) ;
+        res.status(500).json({message : error}) ;
     }
 };
 const updateTask = async (req , res)=>{
     try{
-        await Task.updateOne({name : req.params.id} , {completed : true});
-        res.json({message : "updated"}).status(200) ;
+        const {id : taskID} = req.params ;
+        const task = await Task.findByIdAndUpdate({_id : taskID} , req.body , {
+            new : true ,
+            runValidators : true ,
+        });
+        res.status(200).json(task) ;
     }
     catch(error){
-        res.json({message : error}).status(500) ;
+        res.status(404).json({error_message : `task with id does not exist`}) ;
     }
 };
 module.exports = {
--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose') ;
 
 const TaskSchema = mongoose.Schema({
-    name : String ,
-    completed : Boolean
+    name : {
+        type : String,
+        require : [true , 'name must be provided'],
+        trim : true,
+        maxlength : [20 , 'name is too large'],
+        minlength : [1 , 'cannot store empty string']
+    } ,
+    completed : {
+        type : Boolean,
+        default : false,
+    }
 });
 
 module.exports = mongoose.model('Task' , TaskSchema) ;
