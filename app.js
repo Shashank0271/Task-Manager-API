8a78ca5967108836c53c101aa4af92f63509528b
Shashank Singh <ssingh0271@gmail.com>
2022-12-07 18:34:10 +0530

    used express-async-errors package , removed async wrapper from controller

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express') ;
+require("express-async-errors");
+/*
+If we directly use throw keyword ,(without catching the error and sending it to error 
+handling middleware), its going to break the code (no response , console mei error aa jaega) . --crux
+Thats why we used async wrapper concept .
+This library removes the need of wrapping the controller in async function .
+It allows us to directly throw the error , which whould then be provided to error handling middleware
+If we have not defined any error handling middleware , the default error handler will execute .
+(In this project we have the 'errorHandlerMiddleware' defined)
+*/
+const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks') ;
-const connectDB = require('./db/connect') ;
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 require('dotenv').config();
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -9,21 +19,20 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 const port = 4000;
 
 //middleware :
-app.use(express.json()) ;
-app.use('/api/tasks' , tasks) ;
-app.use(errorHandlerMiddleware) ;
+app.use(express.json());
+app.use('/api/tasks', tasks);
+app.use(errorHandlerMiddleware);
 
-const start = async()=>{
-    try{
-        await connectDB(process.env.MONGO_URI) ;
-        app.listen(port , console.log(`The server is listening on port ${port}...`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`The server is listening on port ${port}...`));
     }
-    catch(error){
-        console.log(error) ;
+    catch (error) {
+        console.log(error);
     }
 }
 start();
-
 
 
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,48 +1,56 @@
-const Task = require('../models/tasks') ;
+const Task = require('../models/tasks');
 const asyncWrapper = require('../middleware/async');
-const { createCustomError } = require('../errors/custom-error');
+const { createCustomError, CustomAPIError } = require('../errors/custom-error');
 
 
-const getAllTasks = asyncWrapper(async (_,res)=>{
-    const result = await Task.find() ;
-    res.status(200).json({result}) ;
-});
+// const getAllTasks = asyncWrapper(async (_,res)=>{
+//     const result = await Task.find() ;
+//     res.status(200).json({result}) ;
+// });
 
-const createTask = asyncWrapper(async (req , res)=>{ 
+const getAllTasks = async (_, res) => {
+    const result = await Task.find();
+    res.status(200).json({ result });
+};
+
+const createTask = async (req, res) => {
     const task = new Task(req.body);
     const newTask = await task.save();
-    res.status(201).json(newTask) ;
-});
+    res.status(201).json(newTask);
+};
 
-const getTask = asyncWrapper(async (req , res , next)=>{
-    const task = await Task.findById({_id : req.params.id}) ;
-    if(!task){
-        return next(createCustomError('Not found' , 404)) ; //goes to error handling middleware , since thats the order of declaration in the app.js file
+const getTask = async (req, res, next) => {
+    const task = await Task.findById({ _id: req.params.id });
+    if (!task) {
+        throw new CustomAPIError('Not found', 404); // we can directly throw it since we use async library it will directly go to the error handling middleware
+        //return next(createCustomError('Not found', 404)); //goes to error handling middleware , since thats the order of declaration in the app.js file
     }
-    res.status(200).json(task) ;
-}
-);
+    res.status(200).json(task);
+};
 
-const deleteTask = asyncWrapper(async(req , res , next)=>{
-    const {id : taskID} = req.params ;
-    const task = await Task.findByIdAndDelete({_id : taskID}) ;
-    if(!task){
-        return next(createCustomError(`no task with id : ${taskID}` , 404)) ;
+const deleteTask = async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+    console.log(task);
+    if (!task) {
+        throw new CustomAPIError(`no task with id : ${taskID}`, 404);
+        // return next(createCustomError(`no task with id : ${taskID}`, 404));
     }
     res.status(204).json();
-});
+};
 
-const updateTask = asyncWrapper(async (req , res , next)=>{
-    const {id : taskID} = req.params ;
-    const task = await Task.findByIdAndUpdate(taskID , req.body , {
-        new : true ,
-        runValidators : true ,
+const updateTask = async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndUpdate(taskID, req.body, {
+        new: true,
+        runValidators: true,
     });
-    if(!task){
-        return next(createCustomError(`document with id ${taskID} does not exist` , 404)) ;
+    if (!task) {
+        throw new CustomAPIError(`document with id ${taskID} does not exist`, 404);
+        // return next(createCustomError(`document with id ${taskID} does not exist`, 404));
     }
-    res.status(200).json(task) ;
-});
+    res.status(200).json(task);
+};
 
 module.exports = {
     getAllTasks,
